a67f198164e06f277a46cb07fc49f911ad7c2887
Tiphaine Surygala <Surygalatiphaine@MBP-de-Tiphaine.home>
2020-08-23 14:56:46 +0100

    creation front end of application with wordpress

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <?php wp_head();?>
+
+    </head>
+
+<body <?php body_class();?>>
+
+<header class="sticky-top">
+
+    <?php wp_nav_menu(
+
+        array(
+            'theme_location' => 'top-menu'
+        )
+        );?>
+    
+</header>
